74c6d1f411c83db774ee8d48f46b9a684366aecb
Guðni Natan Gunnarsson <1493259+GudniNatan@users.noreply.github.com>
2020-02-03 18:46:09 +0000

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameManager instance;
-    public string[] words;
+    public HashSet<string> words;
     public char[][] combos;
     public TextAsset wordsFile;
     public TextAsset comboFile;
@@ -18,11 +18,13 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        words = comboFile.text.Split('\n');
+        words = new HashSet<string> (comboFile.text.Split('\n'));
         combos = new char[words.Length][];
-        for (int i = 0; i < words.Length; i++)
-            combos[i] = words[i].ToCharArray();
-        words = wordsFile.text.Split('\n');
+        int i = 0;
+        foreach (var word in words) {
+            combos[i] = word.ToCharArray();
+            i++;
+        }
     }
 
     // Update is called once per frame
