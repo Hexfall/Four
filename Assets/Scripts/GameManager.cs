5b468ac67764804680eef7d897fc3828d9ade6f7
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2020-02-03 18:00:52 +0000

    Manager and gitignore.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager instance;
+    public string[] words;
+    public char[][] combos;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
