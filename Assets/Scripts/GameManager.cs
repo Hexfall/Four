4fc1d611bf22d629086ad48b406b778b40f232bb
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2020-02-03 19:47:42 +0000

    helper funcs and debug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -23,14 +24,34 @@ public class GameManager : MonoBehaviour
         int i = 0;
         foreach (var word in words) {
             combos[i] = word.ToCharArray();
-            print(word);
             i++;
         }
+        words = new HashSet<string> (wordsFile.text.Split('\n'));
+        foreach (string word in words)
+        {
+            print(word.Length);
+        }
+        foreach (string s in "someMtext".Split('M'))
+            print(s);
+        print(IsWord("WORD"));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public char[] GetCombo()
+    {
+        int index = Random.Range(0, combos.Length);
+        char[] combo = combos[index];
+        char[] shuffledCombo = combo.OrderBy( x => Random.value ).ToArray();
+        return shuffledCombo;
+    }
+
+    public bool IsWord(string word)
+    {
+        return words.Contains(word);
     }
 }
